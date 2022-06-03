c0bbecddf0ed7b514c507d377cd31e079a281417
Rhaz Solomon <rhaz@sharesies.co.nz>
2022-06-03 15:17:59 +1200

    fix: updated several imports

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
-
 import React from "react";
-import MyResume from './pages/MyResume';
-import Cashflow from './pages/Cashflow'
-
 import {
   BrowserRouter as Router,
   Route,
   Routes,
 } from "react-router-dom";
-import LandingPage from './pages/LandingPage';
-import Playground from './pages/Playground';
+
 import CashflowAdmin from './pages/CashflowAdmin';
 import CashflowLogin from './pages/CashflowLogin';
-
 
 function App() {
   return (
